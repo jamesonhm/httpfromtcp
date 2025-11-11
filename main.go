0592ed5b01e475aa583671b90470a1e1599ce8f0
jamesonhm <jamesonhmarshall@gmail.com>
2025-11-10 19:30:24 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer f.Close()
+
+	for {
+		b := make([]byte, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Fprintf(os.Stdout, "read: %s\n", str)
+	}
+}
