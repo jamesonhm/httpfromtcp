250422a55e34fe954c7ebf78efc84a0a0398d38c
jamesonhm <jamesonhmarshall@gmail.com>
2025-11-10 21:51:35 -0700

    complete ch1

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -15,19 +16,41 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
+	linesCh := getLinesChannel(f)
+	for line := range linesCh {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		var currLine string
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currLine != "" {
+					ch <- currLine
+					currLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- fmt.Sprintf("%s%s", currLine, parts[i])
+				currLine = ""
+			}
+			currLine += parts[len(parts)-1]
 		}
-		str := string(b[:n])
-		fmt.Fprintf(os.Stdout, "read: %s\n", str)
-	}
+	}()
+	return ch
 }
